1e20aa36574fb6b8552e63a4fd93fe44cc19dc4f
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-06-22 19:53:54 +0600

    part2start

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 // import CourseInfo from "./CourseInfo.jsx";
 // import State from "./State";
 // import UniCafe from "./UniCafe";
-import Anecdotes from "./Anecdotes";
+// import Anecdotes from "./Anecdotes";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Anecdotes />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
